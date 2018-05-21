7e30a81240453304905058136442fc839c93b6b5
lcherone <lawrence@cherone.co.uk>
2018-05-21 17:45:45 +0000

    add current ui

--- .nuxt/.nuxt/router.js
+++ .nuxt/.nuxt/router.js
@@ -75,7 +75,7 @@ const scrollBehavior = function (to, from, savedPosition) {
 export function createRouter () {
   return new Router({
     mode: 'history',
-    base: '/',
+    base: '/ui/',
     linkActiveClass: 'nuxt-link-active',
     linkExactActiveClass: 'nuxt-link-exact-active',
     scrollBehavior,
